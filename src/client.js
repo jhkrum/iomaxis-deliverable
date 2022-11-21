0ae29eafad82f89da84166c1b6db48f1efee755c
Justin <justinhkrum@gmail.com>
2022-11-21 00:01:38 -0500

    File Transfer Working

--- src/client.js
+++ src/client.js
@@ -1,1 +1,35 @@
-const net = require("net"), fs = require("fs")
+const net = require("net");
+const fs = require("fs");
+
+const customAddr = {
+    "ip" : process.argv[2],
+    "port" : process.argv[3]
+}
+
+let istream = fs.createReadStream("./client-path/Take_Home_Exerecise_4.pdf")
+let socket = new net.Socket();
+
+
+// Default to connecting on port localhost:8000, but can be overriden with custom ip and port
+if(customAddr.port && customAddr.ip){
+    socket.connect(customAddr.port, customAddr.ip, () => {
+        istream.on("readable", () => {
+            let data
+            while (data = this.read()) {
+                socket.write(data)
+            }
+        })
+    })
+} else {
+    socket.connect(8000, 'localhost', () => {
+        istream.on("readable", function() {
+            let data
+            while (data = this.read()) {
+                socket.write(data)
+            }
+        })
+        istream.on("end", function() {
+            socket.end()
+        })
+    })
+}
--- src/server.js
+++ src/server.js
@@ -1,1 +1,25 @@
-const net = require("net"), fs = require("fs")
+const net = require("net")
+const fs = require("fs")
+
+const customAddr = {
+    "ip" : process.argv[2],
+    "port" : process.argv[3]
+}
+
+let ostream = fs.createWriteStream("./server-path/Take_Home_Exerecise_4.pdf")
+
+server = net.createServer(socket => {
+    socket.on('data', chunk => {
+        ostream.write(chunk)
+    })
+    socket.on('end', () => {
+        socket.write("File successfully transfered!")
+    })
+})
+
+// Default to listening on port localhost:8000, but can be overriden with custom ip and port
+if(customAddr.port && customAddr.ip){
+    server.listen(customAddr.port, customAddr.ip)
+} else {
+    server.listen(8000, 'localhost')
+}
